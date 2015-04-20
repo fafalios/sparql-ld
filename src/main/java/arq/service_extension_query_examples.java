0c1fa3b741bcba534b358d0435eb72d78c5c3f29
fafalios <fafalios.pavlos@gmail.com>
2015-04-20 12:18:16 +0300

    Updated licence

--- src/main/java/arq/service_extension_query_examples.java
+++ src/main/java/arq/service_extension_query_examples.java
@@ -1,27 +1,19 @@
 /*
- * 
- * Copyright 2015 FORTH-ICS-ISL (http://www.ics.forth.gr/isl/) 
- * Foundation for Research and Technology - Hellas (FORTH)
- * Institute of Computer Science (ICS) 
- * Information Systems Laboratory (ISL)
- * 
- * Licensed under the EUPL, Version 1.1 or – as soon they
- * will be approved by the European Commission - subsequent 
- * versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the
- * Licence.
- * You may obtain a copy of the Licence at:
- * 
- * http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- * 
- * Unless required by applicable law or agreed to in
- * writing, software distributed under the Licence is
- * distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.
- * See the Licence for the specific language governing
- * permissions and limitations under the Licence.
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package arq;
 
